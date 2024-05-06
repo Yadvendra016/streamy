0d7c8453a70b543954f37c1ffbadd1ecffde8a19
Yadvendra Shukla <yadvendrashukla919@gmail.com>
2024-05-06 17:08:57 +0530

    update recommended service

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -4,11 +4,37 @@ import { getSelf } from "./auth-service";
 export const getRecommended = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  let userId;
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
+
+  let users = [];
+
+  if (userId) {
+    // for logged in user
+    users = await db.user.findMany({
+      where: {
+        // exclude current user
+        NOT: {
+          id: userId,
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  } else {
+    // for log out user
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
 
   return users;
 };
